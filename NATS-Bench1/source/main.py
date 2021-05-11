512530c613186624e56ab95eb1e946213f9156ef
jjaegerman <jonathan.jaegerman@gmail.com>
2021-05-11 11:12:52 -0400

    my turn

--- NATS-Bench1/source/main.py
+++ NATS-Bench1/source/main.py
@@ -3,39 +3,41 @@ import sys
 import random
 import torch
 import glob
+import os
+import math
 import numpy as np
-#from .process import *
+import process
 from nats_bench import create
 from scipy.optimize import minimize_scalar
 from pprint import pprint
-'''
-# Create the API for tologoy search space
-api = create(sys.path[0][0:-7]+'/fake_torch_dir/NATS-tss-v1_0-3ffb9-full', 'tss', fast_mode=True, verbose=False)
-dataset = 'cifar10'
-hp = '200'
-
-#loop through all files{
-info = api.get_more_info(531, dataset, hp='200', is_random=False)
-params = api.get_net_param(531, 'cifar10', None)
-get_quality(i)
-increment_correlarion()
-}
 
 
 def get_quality(model_params):
     for i in model_params:
-        #Gets main dictionary key
-        for k, v in model_params[i].items():
-            if('weight' in k and len(list(v.size())) == 4):
+        for k, v in (model_params[i]).items():
+            quality_list = []
+            if('weight' in k and len(list(v.size())) == 4 and v.shape[3]!=1):
                 print(k)
-                rank, KG, condition = get_metrics(model_params,i,k)
-                #accumulatequality
                 print("\n")
+                rank, KG, condition = process.get_metrics(model_params,i,k)
+                try:
+                    quality_list.append(math.atan(KG/(1-1/condition)))
+                except:
+                    quality_list.append(0)
 
 
-def accumulate_quality(model_quality)
-'''
-api = create(sys.path[0][0:-7]+'/fake_torch_dir/NATS-tss-v1_0-3ffb9-full', 'tss', fast_mode=True, verbose=False)
-dataset = 'cifar10'
-hp = '200'
-info = api.get_more_info('000531', dataset, hp, is_random=False)
+if __name__ == "__main__":
+    api = create(sys.path[0][0:-7]+'/fake_torch_dir/NATS-tss-v1_0-3ffb9-full', 'tss', fast_mode=True, verbose=False)
+    dataset = 'cifar10'
+    hp = '200'
+
+    pickles=glob.glob(sys.path[0][0:-7]+'/fake_torch_dir/NATS-tss-v1_0-3ffb9-full/*')
+    model_qualities = []
+
+    for model in pickles:
+        model_num = int((model.split(os.path.sep)[-1]).split('.')[0])
+        print(model_num)
+        info = api.get_more_info(model_num, dataset, hp=hp, is_random=False)
+        params = api.get_net_param(model_num, dataset, None)
+        get_quality(params)
+        #increment_correlarion()
