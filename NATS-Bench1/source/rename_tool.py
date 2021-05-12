b8fa4f7bde30b63b3419f30aa24578328f2ba6c1
jjaegerman <jonathan.jaegerman@gmail.com>
2021-05-12 09:51:30 -0400

    added effective rank

--- NATS-Bench1/source/rename_tool.py
+++ NATS-Bench1/source/rename_tool.py
@@ -2,10 +2,10 @@ import glob
 import sys
 import os
 
-pickles = glob.glob(sys.path[0][0:-7]+'/fake_torch_dir/NATS-tss-v1_0-3ffb9-full/*')
+pickles = glob.glob(sys.path[0][0:-7]+'/fake_torch_dir/models/*')
 for pickle in pickles:
     try:
         print(str(int((pickle.split(os.path.sep)[-1]).split('.')[0])))
-        os.rename(pickle,("NATS-Bench1/fake_torch_dir/NATS-tss-v1_0-3ffb9-full/"+str(int((pickle.split(os.path.sep)[-1]).split('.')[0]))+".pickle.pbz2"))
+        os.rename(pickle,("NATS-Bench1/fake_torch_dir/models/"+str(int((pickle.split(os.path.sep)[-1]).split('.')[0]))+".pickle.pbz2"))
     except:
         print("skipping")
