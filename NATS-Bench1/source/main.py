4a7eed49ebe9e9388a0f4cfd064d432977c48a52
Khalil Damouni <KhalilJDamouni@gmail.com>
2021-05-12 10:38:29 -0400

    added debug

--- NATS-Bench1/source/main.py
+++ NATS-Bench1/source/main.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     api = create(sys.path[0][0:-7]+'/fake_torch_dir/models', 'tss', fast_mode=True, verbose=False)
     dataset = 'cifar10'
     hp = '200'
-    early_stop = 5   
+    early_stop = 500000000000
     i = 0
 
     pickles=glob.glob(sys.path[0][0:-7]+'/fake_torch_dir/models/*')
